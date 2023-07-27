eebbd85c5a9d11bf849f3955c382da5a3200acbf
100daggers <dinesh.soudagar@gmail.com>
2023-07-27 14:35:23 +0200

    Added docu

--- utils/extract_BDD_images_list.py
+++ utils/extract_BDD_images_list.py
@@ -1,3 +1,6 @@
+"""
+This file has the information about the BDD 100K images. And a method to extract a specific list required image names.
+"""
 import json
 from tqdm import tqdm
 from pathlib import Path
@@ -5,7 +8,7 @@ from pathlib import Path
 train_labels_path = "C:/Others/BDD/bdd100k_labels_release/bdd100k\labels/bdd100k_labels_images_train.json"
 val_labels_path = "C:/Others/BDD/bdd100k_labels_release/bdd100k\labels/bdd100k_labels_images_val.json"
 
-# list of weather types
+# list of weather types in BDD
 types_of_weather_list = ['clear', 'rainy', 'undefined', 'snowy', 'overcast', 'partly cloudy', 'foggy']
 types_of_weather_count_train = {'clear': 37344, 'rainy': 5070, 'undefined': 8119, 'snowy': 5549, 'overcast': 8770,
                                 'partly cloudy': 4881, 'foggy': 130}
